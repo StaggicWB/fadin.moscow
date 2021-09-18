9de4b3f1c27f58ab80b214d2f33d34deead2d20b
Staggic.web <63167627+StaggicWB@users.noreply.github.com>
2021-09-18 10:10:57 +0300

    Add files via upload

---
+++ js/infotab.js
@@ -0,0 +1,21 @@
+//Modal Script//
+
+let modal = document.getElementById("modalinfo_right");
+
+let btn = document.getElementById("btn_info");
+
+let span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
---
+++ js/modal.js
@@ -0,0 +1,21 @@
+//Modal Script//
+
+let modal = document.getElementById("modalshowreel_left");
+
+let btn = document.getElementById("showreel_button");
+
+let span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
